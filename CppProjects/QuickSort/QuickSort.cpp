567d40317c2b6dc4a445259fb7ccdc01a74e2124
FridayJarvis <charsovden@gmail.com>
2025-03-24 14:01:32 +0300

    Created quick sort on C++

---
+++ CppProjects/QuickSort/QuickSort.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int partion(std::vector<int>& vec, int low, int high) {
+	int pivot = vec[high];
+
+	int i = low - 1;
+
+	for (int j = low; j <= high - 1; ++j) {
+		if (vec[j] <= pivot) {
+			i++;
+			std::swap(vec[i], vec[j]);
+		}
+	}
+
+	std::swap(vec[i + 1], vec[high]);
+
+	return i + 1;
+}
+
+void quick_sort(std::vector<int>& vec, int low, int high) {
+	if (low < high) {
+		int pi = partion(vec, low, high);
+
+		quick_sort(vec, low, pi - 1);
+		quick_sort(vec, pi + 1, high);
+	}
+}
+
+int main() {
+	srand(time(0));
+	std::vector<int> vec = { 3, 1, 4, 2, 3 };
+
+	quick_sort(vec, 0, vec.size() - 1);
+	return 0;
+}
